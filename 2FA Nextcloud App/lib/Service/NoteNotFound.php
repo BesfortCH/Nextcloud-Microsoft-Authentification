bf73c431de3af986d0ce3baa178338baf7d36a80
BesfortCH <106925273+BesfortCH@users.noreply.github.com>
2024-02-21 13:39:22 +0100

    Add files via upload

---
+++ 2FA Nextcloud App/lib/Service/NoteNotFound.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+// SPDX-FileCopyrightText: Besfort Ajeti <Besfortajeti915@gmail.com>
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+namespace OCA\MicrosoftMFA\Service;
+
+class NoteNotFound extends \Exception {
+}
